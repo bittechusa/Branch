5b73c3d04ed3d727e5a653fc51413bd60fbdbbb5
shahidul <shahidulmail@gmail.com>
2016-04-03 12:54:10 -0400

    add

--- GitPractice/src/test/java/Demo.java
+++ GitPractice/src/test/java/Demo.java
@@ -3,5 +3,38 @@ public class Demo
 {
 	//to clone this repo
 	//git clone https://github.com/bittechusa/Branch.git
+	
+	//how to upload  in github
+	//1.go to repo label
+//	2.git add -A
+//	3.git commit -m "any comment"
+//	4.git pull or git pull origin branch name
+//	5.git push origin branch name
+	
+	//to pull anything from repo
+//	1.go to repo label
+//	2.git pull origin branch name
+	
+	//commonly used command 
+//	git status---> to check everything commited or not
+//	git branch---> to check which branch
+//	git branch branch name----> to create branch
+//	git checkout branchName---> to switch the branch
+//	git checkout -b branchName---> to switched branch with creating branch
+//	git log----> to see log 
+//	git merge branch name
+	
+	
+	//to get prepo from github server
+	// git clone https://github.com/bittechusa/Branch.git
+	
+	
+	//to import project from repo to eclipse:
+	//click file-->click import-->existing maven project if project is maven or existing project into workspace
+	//-->click next-->browse the repo-->finish
+	
+	
+	to export a project into repo
+	//click file-->click export-->click file system-->click next-->browse the repo-->click finish
 
 }
